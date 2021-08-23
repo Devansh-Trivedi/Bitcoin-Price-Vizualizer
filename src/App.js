bc44bc9405fe485846eb62ec237cec2811a0b01d
Devansh-Trivedi <tridevansh.160601@gmail.com>
2021-08-23 07:40:29 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ import Tron from './Tron';
         <Route path="/Litecoin">
           <Litecoin />
         </Route>
-        <Route exact path="/Tron">
+        <Route path="/Tron">
           <Tron />
         </Route>        
       </Switch>
